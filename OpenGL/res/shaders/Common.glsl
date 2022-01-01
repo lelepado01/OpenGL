aad562e51129de6d2064c4478dfc5bd8b11e2026
Gabriele Padovani <gabriele.padovani@gmail.com>
2022-01-01 10:33:10 +0100

    color  of terrain also depends on normal angle

--- OpenGL/res/shaders/Common.glsl
+++ OpenGL/res/shaders/Common.glsl
@@ -14,3 +14,9 @@ struct Light {
     vec4 diffuse;
     vec4 specular;
 };
+
+const float M_PI = 3.14159265359; 
+
+float magnitude(vec3 v){
+    return sqrt( pow(v.x,2) + pow(v.y,2) + pow(v.z,2));
+}
